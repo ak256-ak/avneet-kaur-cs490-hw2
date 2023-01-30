15bb1b5f31e3a74c398c1da00cd3502d89337d2c
ak256-ak <ak256@njit.edu>
2023-01-29 22:31:41 -0500

    commit 7

--- web/src/layouts/BlogLayout/BlogLayout.js
+++ web/src/layouts/BlogLayout/BlogLayout.js
@@ -1,43 +1,38 @@
-import { useAuth } from '@redwoodjs/auth'
-import { Link, routes } from '@redwoodjs/router'
-
+import {useAuth} from '@redwoodjs/auth'
+import { Link, routes} from '@redwoodjs/router'
 const BlogLayout = ({ children }) => {
-  const { isAuthenticated, currentUser, logOut } = useAuth()
-
+  const { isAuthenticated, currentUser, logOut} =useAuth()
   return (
-    <>
-      <header>
-        <div className="flex-between">
-          <h1>
-            <Link to={routes.home()}>Avneet HomePage</Link>
-          </h1>
-          {isAuthenticated ? (
-            <div>
-              <span>Logged in as {currentUser.email}</span>{' '}
-              <button type="button" onClick={logOut}>
-                Logout
-              </button>
-            </div>
-          ) : (
-            <Link to={routes.login()}>Login</Link>
-          )}
+  <>
+    <header>
+      <div className = "flex-between">
+      <h1>
+        <Link to= {routes.home()}>Avneet Homepage</Link>
+      </h1>
+      {isAuthenticated ? (
+        <div>
+          <span>Logged in as {currentUser.email}</span>{''}
+          <button type = "button" onClick = {logOut}>
+            Logout
+          </button>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <Link to={routes.home()}>Home</Link>
-            </li>
-            <li>
-              <Link to={routes.about()}>About</Link>
-            </li>
-            <li>
-              <Link to={routes.contact()}>Contact</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <main>{children}</main>
-    </>
+      ) : (
+        <Link to={routes.login()}>Login</Link>
+      )}
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link to={routes.about()}>About</Link>
+          </li>
+          <li>
+            <Link to={routes.contact()}>Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>{children}</main>
+  </>
   )
 }
 
