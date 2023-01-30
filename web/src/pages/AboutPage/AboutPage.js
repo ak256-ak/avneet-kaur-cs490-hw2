834a2e14ccf49dc6cc88a98eb4e20c6a4e412170
ak256-ak <ak256@njit.edu>
2023-01-29 21:48:35 -0500

    commit 3

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <p>This site was created as a Test!!!</p>
+    </>
+  )
+}
+
+export default AboutPage
